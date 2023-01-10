0d1a7096706bae08f06e2e32dbc4f88970fb7d84
J. Aaron Crick <j.crick@mailfence.com>
2023-01-10 14:27:13 +0100

    More formatting

--- src/em_notes/views.cljs
+++ src/em_notes/views.cljs
@@ -6,14 +6,12 @@
    [em-notes.views.about :as about]
    ))
 
-(defn page [page-name]
-  (js/console.log "page-name:" page-name)
+(defn page [page-name] 
   (case page-name
     :home-panel [home/home-panel]
     :about-panel [about/about-panel]
     [home/home-panel]))
   
-
 (defn main-panel []
   (let [active-panel (re-frame/subscribe [::subs/active-panel])]
     (page @active-panel)))
