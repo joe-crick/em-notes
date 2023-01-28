afd2b7758b8821a6f4e4cbcfdb70156d865a9003
J. Aaron Crick <j.crick@mailfence.com>
2023-01-28 17:35:03 +0100

    Improve the look of the loading page. Add a static progress bar.
    Reported-by: J. Aaron Crick

--- src/em_notes/views/loading.cljs
+++ src/em_notes/views/loading.cljs
@@ -2,4 +2,7 @@
 
 (defn loading-splash []
   [:div.container
-   [:h1 {:class "title is-1"} "Loading..."]])
+   [:h1 {:class "title is-1"} "Loading..."]
+   [:progress
+    {:class "progress is-success", :value "90", :max "100"}
+    "60%"]])
