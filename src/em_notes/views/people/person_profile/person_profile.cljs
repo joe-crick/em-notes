618c8ce38bcbc6f08e5a31ba5bf4751828ca6bbd
J. Aaron Crick <j.crick@mailfence.com>
2023-02-02 15:36:33 +0100

    Fix i18n bug on person profile. Wrong text reference.
    Reported-by: J. Aaron Crick

--- src/em_notes/views/people/person_profile/person_profile.cljs
+++ src/em_notes/views/people/person_profile/person_profile.cljs
@@ -1,8 +1,7 @@
 (ns em-notes.views.people.person-profile.person-profile
   (:require [em-notes.i18n.tr :refer [grab]]
             [em-notes.components.section-toggle :refer [section-toggle]]
-            [em-notes.components.fields.text-input :refer [set-text-input]]
-            [em-notes.components.fields.number-input :refer [set-number-input]]
+            [em-notes.components.fields.text-input :refer [set-text-input]] 
             [em-notes.components.fields.select :refer [set-select]]))
 
 (defn profile [person revise!] 
@@ -11,7 +10,7 @@
     (fn []
       [:fieldset
        [:legend (grab :person/details)]
-       [select {:label (grab :growth-metric/progress)
+       [select {:label (grab :person/attachment-style)
                 :property [:attachment-style]
                 :values [[nil (grab :person/secure)]
                          [nil (grab :person/anxious)]
