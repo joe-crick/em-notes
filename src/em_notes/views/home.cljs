78674cb6bda6b3ed95587cdedf12bc73801f065b
J. Aaron Crick <j.crick@mailfence.com>
2023-02-03 08:45:23 +0100

    Fix bug in Home page title.
    Reported-by: J. Aaron Crick

--- src/em_notes/views/home.cljs
+++ src/em_notes/views/home.cljs
@@ -1,19 +1,15 @@
 (ns em-notes.views.home
   (:require [em-notes.components.tabbed-view :refer [tabbed-view]]
             [em-notes.i18n.tr :refer [grab]]
-            [em-notes.subs :as subs]
             [em-notes.views.people.people :refer [people]]
-            [em-notes.views.teams.teams :refer [teams]]
-            [re-frame.core :as rf]))
+            [em-notes.views.teams.teams :refer [teams]]))
 
 (defn home []
-  (let [active-person (rf/subscribe [::subs/active-person])
-        title (str (:first-name @active-person) " " (:last-name @active-person))]
-    (fn []
-      [:div
-       [tabbed-view {:tab-navs [[:people (grab :people/title)]
-                                [:teams (grab :teams/title)]]
-                     :views {:people people
-                             :teams teams}
-                     :action-buttons []
-                     :title title}]])))
+  (fn []
+    [:div
+     [tabbed-view {:tab-navs [[:people (grab :people/title)]
+                              [:teams (grab :teams/title)]]
+                   :views {:people people
+                           :teams teams}
+                   :action-buttons []
+                   :title (grab :home/home)}]]))
